235433968379f9d93b4165b2001972377f5593f4
Anicai Ovidiu <ovi@0v1.ro>
2020-12-26 10:53:14 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(500)
-    basic.pause(1000)
+    basic.pause(Pause)
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(-500)
-    basic.pause(1000)
+    basic.pause(Pause)
 })
+let Pause = 0
 radio.setGroup(1)
+Pause = 100
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
 	
