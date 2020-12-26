3f9aaee345e1800c7404cc7bb8d32513dae5edcf
Anicai Ovidiu <ovi@0v1.ro>
2020-12-26 11:57:43 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,10 @@ input.onButtonPressed(Button.A, function () {
     radio.sendNumber(500)
     basic.pause(Pause)
 })
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("A")
+    basic.pause(Pause)
+})
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(-500)
     basic.pause(Pause)
@@ -11,5 +15,10 @@ radio.setGroup(1)
 Pause = 100
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-	
+    if (input.rotation(Rotation.Roll) < 0) {
+        radio.sendNumber(input.rotation(Rotation.Roll) * 10)
+    } else {
+        radio.sendNumber(input.rotation(Rotation.Roll) * 10)
+    }
+    basic.pause(Pause)
 })
